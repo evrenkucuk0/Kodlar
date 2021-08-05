99775569b2fe77dd6b89e11f608c0b231206d7eb
evrenkucuk0 <80481477+evrenkucuk0@users.noreply.github.com>
2021-08-05 13:34:09 +0300

    Add files via upload

---
+++ fibonacci.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+	int n;
+	printf ("matrisin boyutunu giriniz");
+	scanf("%d",&n);
+	int a;
+	int b;
+	int c;	
+	int i;
+	a=1;
+	b=1;
+	for (i=0; i<n;i++){
+		printf("%d,",a);
+		c = a + b ;
+		a=b;
+		b=c;
+	}
+}
