a07e315aaf2259e2066082fd0b6a2bda81b667eb
evrenkucuk0 <80481477+evrenkucuk0@users.noreply.github.com>
2021-08-06 09:59:17 +0300

    Add files via upload

---
+++ Ortalama hesaplama.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	
+	
+	float a, b, ortalama;
+	printf("iki sayi giriniz>");
+	scanf("%f%f",&a,&b); // %f'nin yanýna \n koyunca3. bir deðer istiyor
+	ortalama= (a+b)/2;
+	printf("ortalama:%f'dir",ortalama);
+	
+}
