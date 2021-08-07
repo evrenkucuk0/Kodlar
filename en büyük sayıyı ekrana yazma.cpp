b475adffffaacda55d4f9483d312885b494e9abd
evrenkucuk0 <80481477+evrenkucuk0@users.noreply.github.com>
2021-08-07 10:38:53 +0300

    Add files via upload

---
+++ en büyük sayıyı ekrana yazma.cpp
@@ -0,0 +1,20 @@
+//4. dersin 2. sorusu yapýlmadý
+
+//algoritmayý oluþturmak sýkýntý oluyo
+//bir örnekten sonra daha rahat oluyo algoritmayý
+//oluþturmak
+#include<stdio.h>
+int main(){
+	int sayi_1,sayi_2,sayi_3;
+	printf("uc sayi giriniz");
+	scanf("%d%d%d",&sayi_1,&sayi_2,&sayi_3);
+	if(sayi_1>=sayi_2&&sayi_1>=sayi_3)
+		printf("%d en buyuk sayidir\n",sayi_1);
+	else 
+	if(sayi_2>=sayi_1&&sayi_2>=sayi_3)
+		printf("%d en buyuk sayidir\n",sayi_2);
+	else
+	if(sayi_3>=sayi_1&&sayi_3>=sayi_2)
+		printf("%d en buyuk sayidir\n",sayi_3);
+		
+}
