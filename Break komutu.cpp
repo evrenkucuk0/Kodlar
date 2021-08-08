268b6c40afd2164c7d0d216c27b0ccc8992ff3f6
evrenkucuk0 <80481477+evrenkucuk0@users.noreply.github.com>
2021-08-08 15:14:04 +0300

    Add files via upload

---
+++ Break komutu.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	int i,tesadufi_sayi;
+	int deneme_sayisi=0;
+	while (1){
+		tesadufi_sayi= rand () % 100;
+		deneme_sayisi++;
+		if(tesadufi_sayi==61)break;
+	}
+printf("Toplam deneme sayisi: %d\n",deneme_sayisi);
+	
+}
+// rand satýrý hatalý diyo
+//nette baktým kütüphane deðiþ felan diyo
