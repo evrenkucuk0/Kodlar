a4e739695b98c87c3ecbf126d5974da7dba13086
evrenkucuk0 <80481477+evrenkucuk0@users.noreply.github.com>
2021-08-09 13:35:30 +0300

    Update Ders3Örnek2.cpp

--- Ders3Örnek2.cpp
+++ Ders3Örnek2.cpp
@@ -5,13 +5,12 @@ int main(){
 	printf("2 tam sayi giriniz");
 	scanf("%d%d%f"&bolunen,&bolen,&sonuc);
 	if (bolen!=0){
-		//burdan sonrası bakarak yapabildim
 		sonuc=(float)bolunen/bolen;
 		printf("sonuc:%.2f\n",sonuc);
 	}
 	else
-	 printf("hata: sayı 0'a bolunemez\n");
+	 printf("hata: sayÄ± 0'a bolunemez\n");
 	
 	
-	//çalışmıyo hata ne
+	//Ã§alÄ±ÅŸmÄ±yo hata ne
 }
