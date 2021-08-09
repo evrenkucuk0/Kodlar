85687d957ed4d10d7faa7ec47b8fbf45af26ad32
evrenkucuk0 <80481477+evrenkucuk0@users.noreply.github.com>
2021-08-09 13:01:17 +0300

    Update Virgüllü sayıyı tam sayıya çevirme.cpp

--- Virgüllü sayıyı tam sayıya çevirme.cpp
+++ Virgüllü sayıyı tam sayıya çevirme.cpp
@@ -4,6 +4,5 @@ int main(){
 	printf("Lutfen bir sayi giriniz>");
 	scanf("%f",&girilen_sayi);
 	printf("sayinin yuvarlanmis hali:%d\n",(int)(girilen_sayi+0.5));
-	// Burada 0.5'in görevi nedir(amacý +0.5 i yukarýya yuvarlamak ama olmuyo)
-	// kodu açtýðýmda 4,5'i bazen 4 bazen 5 olarak kabul ediyo
+
 }
