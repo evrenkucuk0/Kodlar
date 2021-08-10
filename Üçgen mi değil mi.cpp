4edcecab9facfbae8ee8a8fe0efb03e7b256be56
evrenkucuk0 <80481477+evrenkucuk0@users.noreply.github.com>
2021-08-10 14:29:20 +0300

    Update Üçgen mi değil mi.cpp

--- Üçgen mi değil mi.cpp
+++ Üçgen mi değil mi.cpp
@@ -2,19 +2,22 @@
 int main(){
 	int a,b,c;
 	int temp;
-	printf("birinci kenar uzunlugu>");
+	printf("birinci kenar uzunlugu>\n");
 	scanf("%d",&a);
-	printf("ikinci kenar uzunlugu>");
+	printf("ikinci kenar uzunlugu>\n");
 	scanf("%d",&b);
-	printf("ucuncu kenar uzunlugu>");
+	printf("ucuncu kenar uzunlugu>\n");
 	scanf("%d",&c);
 	if(a < b){
 		temp = a;
 		a = b;
 		b = temp;
 	}
-		if( ((a + b) < c) || ((a - b) > c) )
+		if( ((a + b) < c) || ((a - b) > c) ){
 		printf("Bu kenar uzunluklarina sahip bir ucgen olamaz\n");
-	else
+		}
+	else{
 		printf("Bu kenar uzunluklarina sahip bir ucgen cizilebilir\n");
+	}
+			
 }
