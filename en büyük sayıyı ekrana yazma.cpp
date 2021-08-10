64fde36f0ece2ef4de114dd41a6ce0d47d3578bd
evrenkucuk0 <80481477+evrenkucuk0@users.noreply.github.com>
2021-08-10 15:05:34 +0300

    Update en büyük sayıyı ekrana yazma.cpp

--- en büyük sayıyı ekrana yazma.cpp
+++ en büyük sayıyı ekrana yazma.cpp
@@ -7,11 +7,9 @@ int main(){
 	scanf("%d%d%d",&sayi_1,&sayi_2,&sayi_3);
 	if(sayi_1>=sayi_2&&sayi_1>=sayi_3)
 		printf("%d en buyuk sayidir\n",sayi_1);
-	else 
-	if(sayi_2>=sayi_1&&sayi_2>=sayi_3)
+	else if(sayi_2>=sayi_1&&sayi_2>=sayi_3)
 		printf("%d en buyuk sayidir\n",sayi_2);
-	else
-	if(sayi_3>=sayi_1&&sayi_3>=sayi_2)
+	else if(sayi_3>=sayi_1&&sayi_3>=sayi_2)
 		printf("%d en buyuk sayidir\n",sayi_3);
 		
 }
