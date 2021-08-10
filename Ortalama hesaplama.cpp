c6b2bde960b0147ac6eaa89bbf5f8c4343d12fea
evrenkucuk0 <80481477+evrenkucuk0@users.noreply.github.com>
2021-08-10 10:30:33 +0300

    Update Ortalama hesaplama.cpp

--- Ortalama hesaplama.cpp
+++ Ortalama hesaplama.cpp
@@ -4,7 +4,7 @@ int main(){
 	
 	float a, b, ortalama;
 	printf("iki sayi giriniz>");
-	scanf("%f%f",&a,&b); // %f'nin yanýna \n koyunca3. bir deðer istiyor
+	scanf("%f%f",&a,&b); 
 	ortalama= (a+b)/2;
 	printf("ortalama:%f'dir",ortalama);
 	
