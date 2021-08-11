902f6f29e137a95eabe44f8ac3f5d1b00d06084a
evrenkucuk0 <80481477+evrenkucuk0@users.noreply.github.com>
2021-08-11 10:47:47 +0300

    Update Break komutu.cpp

--- Break komutu.cpp
+++ Break komutu.cpp
@@ -1,11 +1,12 @@
-
 #include<stdio.h>
 #include<stdlib.h>
+#include<time.h>
 int main(){
 	int i,tesadufi_sayi;
 	int deneme_sayisi=0;
+	srand(time(NULL));
 	while (1){
-		tesadufi_sayi= rand () % 100;
+		tesadufi_sayi= rand() % 100;
 		deneme_sayisi++;
 		if(tesadufi_sayi==61)break;
 	}
